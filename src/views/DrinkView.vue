b43289fd03ff1a8e6020c773d79756f2574acdf7
Vivi <vachetvirginie@gmail.com>
2023-07-10 03:21:06 +0200

    wip

--- src/views/DrinkView.vue
+++ src/views/DrinkView.vue
@@ -11,7 +11,7 @@
           <v-card-text>
             <div class="text-h2 pa-12">
               <v-sheet width="300" class="mx-auto">
-                <v-form fast-fail @submit.prevent="saveMilkDrink">
+                <v-form fast-fail @submit.prevent="saveMilkDrink()">
                   <v-text-field v-model="currentDate"></v-text-field>
 
                   <v-text-field v-model="currentTime"></v-text-field>
@@ -87,10 +87,15 @@
 <script>
 import { ref, onMounted, computed } from "vue";
 import { useStore } from "vuex";
-import { db } from "@/utils/useFirebase";
-import { setDoc, doc, getDocs, collection } from "firebase/firestore";
-import { format } from "date-fns";
-import { fr } from "date-fns/locale"; // Import the French locale module
+import {
+  saveMilkDrink,
+  getBiberons,
+  aggregateQuantities,
+  formatDate,
+  getColor,
+  getBarHeight,
+} from "@/utils/api";
+import { fr } from "date-fns/esm/locale";
 
 export default {
   setup() {
@@ -100,9 +105,6 @@ export default {
     const store = useStore();
     const userId = computed(() => store.state.userId);
     const lastBiberons = ref([]);
-    const isActive = ref(false);
-    const targetValue = ref(660);
-    const maxHeight = ref(100);
     const aggregatedBiberons = ref();
     const frenchLocale = fr;
 
@@ -121,108 +123,39 @@ export default {
       return now.toLocaleTimeString();
     }
 
-    function saveMilkDrink() {
-      setDoc(
-        doc(
-          db,
-          "biberons",
-          (userId.value, Math.random() + 1).toString(36).substring(4)
-        ),
-        {
-          userId: userId.value,
-          quantity: parseInt(quantity.value),
-          date: currentDate.value,
-          time: currentTime.value,
-        },
-        { merge: true }
-      ).then(() => {
-        quantity.value = 0;
-        getBiberons().then((biberons) => {
-          lastBiberons.value = biberons.filter((biberon) => {
-            return biberon.userId === userId.value;
-          });
-        });
-        isActive.value = false;
-      });
-    }
-
-    async function getBiberons() {
-      const biberons = [];
-      const querySnapshot = await getDocs(collection(db, "biberons"));
-      querySnapshot.forEach((doc) => {
-        biberons.push(doc.data());
-      });
-      return biberons;
-    }
-
-    function getBarHeight(quantity) {
-      const height = (quantity / targetValue.value) * maxHeight.value;
-      return height + "px";
-    }
-
-    function getColor(quantity) {
-      let color = "";
-
-      if (quantity >= 0 && quantity <= 250) {
-        color = "red";
-      } else if (quantity > 250 && quantity <= 500) {
-        color = "orange";
-      } else if (quantity > 500 && quantity <= 600) {
-        color = "yellow";
-      } else {
-        color = "green";
-      }
-
-      return color;
-    }
-
-    function aggregateQuantities(biberons) {
-      const quantitiesMap = new Map();
-
-      for (const biberon of biberons) {
-        const { date, quantity } = biberon;
-
-        if (quantitiesMap.has(date)) {
-          quantitiesMap.set(date, quantitiesMap.get(date) + quantity);
-        } else {
-          quantitiesMap.set(date, quantity);
-        }
-      }
-
-      const aggregatedBiberons = [];
-      quantitiesMap.forEach((quantity, date) => {
-        aggregatedBiberons.push({ date, quantity });
-      });
-
-      return aggregatedBiberons;
-    }
-
     onMounted(() => {
-      getBiberons().then((biberons) => {
-        lastBiberons.value = biberons.filter((biberon) => {
-          return biberon.userId === userId.value;
-        });
+      getBiberons(userId.value).then((biberons) => {
+        lastBiberons.value = biberons;
         aggregatedBiberons.value = aggregateQuantities(lastBiberons.value);
       });
     });
-
-    function formatDate(date, formatStr, locale) {
-      return format(new Date(date), formatStr, { locale });
-    }
 
     return {
       quantity,
       currentDate,
       currentTime,
-      saveMilkDrink,
+      userId,
+      saveMilkDrink: () => {
+        saveMilkDrink(
+          userId.value,
+          quantity.value,
+          currentDate.value,
+          currentTime.value
+        ).then(() => {
+          getBiberons(userId.value).then((biberons) => {
+            lastBiberons.value = biberons;
+            aggregatedBiberons.value = aggregateQuantities(lastBiberons.value);
+          });
+        });
+      },
       lastBiberons,
-      getBarHeight,
-      getColor,
       aggregatedBiberons: computed(() =>
         aggregateQuantities(lastBiberons.value)
       ),
       formatDate,
       frenchLocale,
+      getColor,
+      getBarHeight,
     };
   },
 };
